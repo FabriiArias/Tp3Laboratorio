9eee151c16bc7381018dc070353243aa10dee756
Fabrizio Arias <fabrisal@live.com.ar>
2024-03-26 17:07:20 -0300

    Initial commit

---
+++ src/tp3laboratorio/Tp3Laboratorio.java
@@ -0,0 +1,13 @@
+package tp3laboratorio;
+
+public class Tp3Laboratorio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+    }
+
+}
